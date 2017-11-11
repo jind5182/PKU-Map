23caaf32d7ec2e69dc8101cf3cab33be7fb20a5e
tonywyb <1500012946@pku.edu.cn>
2017-11-11 22:44:59 +0800

    GetInfo for login

--- app/src/main/java/com/example/macpro/pku_map/Login.java
+++ app/src/main/java/com/example/macpro/pku_map/Login.java
@@ -1,5 +1,6 @@
 package com.example.macpro.pku_map;
 
+import android.util.*;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,11 +9,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.content.*;
+import java.io.*;
 
+import org.apache.http.protocol.HTTP;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.apache.http.entity.ByteArrayEntity;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.BaseJsonHttpResponseHandler;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 import org.apache.http.Header;
+import org.apache.http.Consts;
+import org.apache.http.message.BasicHeader;
 
 public class Login extends Activity implements View.OnClickListener {
 
@@ -21,6 +32,7 @@ public class Login extends Activity implements View.OnClickListener {
     private Button signinlater = null;
     private EditText username = null;
     private EditText passwd = null;
+    Context mContext = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +51,51 @@ public class Login extends Activity implements View.OnClickListener {
         signinlater.setOnClickListener(this);
     }
 
+    private void loginByAsyncHttpClientPost(String userName, String userPass) {
+        //创建异步请求对象
+        AsyncHttpClient client = new AsyncHttpClient();
+        //输入要请求的url
+        String url = "http://120.25.232.47:8001/login/";
+        //String url = "http://www.baidu.com";
+        //请求的参数对象
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("username",userName);
+            jsonObject.put("password",userPass);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        //将参数加入到参数对象中
+        ByteArrayEntity entity = null;
+        try {
+            entity = new ByteArrayEntity(jsonObject.toString().getBytes("UTF-8"));
+            entity.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        //进行post请求
+        client.post(mContext, url, entity, "application/json", new JsonHttpResponseHandler() {
+            //如果成功
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                Toast.makeText(Login.this, "status code is:"+ statusCode+ "connection success!"+response.toString(), Toast.LENGTH_LONG).show();
+                super.onSuccess(statusCode, headers, response);
+                //Log.e("rs",response.toString());
+                //Toast.makeText(mContext, "connection success!"+response.toString(), Toast.LENGTH_SHORT).show();
+                //System.out.println("response: " + response);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Toast.makeText(mContext, "connection error!Error number is:" + statusCode,  Toast.LENGTH_SHORT).show();
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+                //Toast.makeText(mContext, "connection error!Error number is:" + statusCode,  Toast.LENGTH_SHORT).show();
+            }
+        });
+        return;
+
+    }
+
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
@@ -51,8 +108,7 @@ public class Login extends Activity implements View.OnClickListener {
                 else {
                     String[] param = {Username, Passwd};
                     TextView displaytxt = (TextView) findViewById(R.id.display_txt);
-                    GetInfo task = new GetInfo(displaytxt);
-                    task.loginByAsyncHttpClientPost(param[0], param[1]);
+                    loginByAsyncHttpClientPost(param[0], param[1]);
                     Toast.makeText(this, "用户名：" + Username + " 密码为：" + Passwd, Toast.LENGTH_SHORT).show();
                     startActivity(new Intent(Login.this, MainActivity.class));
                     finish();
