d50401832ce40a7745ea0009eecb9a7d47129fed
tonywyb <1500012946@pku.edu.cn>
2017-11-29 21:03:08 +0800

    GetInfo for login

--- app/src/main/java/com/example/macpro/pku_map/Signup.java
+++ app/src/main/java/com/example/macpro/pku_map/Signup.java
@@ -11,6 +11,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.content.*;
 import java.io.*;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+
 import org.apache.http.protocol.HTTP;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -27,6 +30,27 @@ public class Signup extends Activity{
     private EditText suusername, supasswd, supasswd2, suid, suphonenumber;
     Context mContext = this;
 
+    public static String getMD5(String str) {
+        try {
+            // 生成一个MD5加密计算摘要
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            // 计算md5函数
+            md.update(str.getBytes());
+            // digest()最后确定返回md5 hash值，返回值为8为字符串。因为md5 hash值是16位的hex值，实际上就是8位的字符
+            // BigInteger函数则将8位的字符串转换成16位hex值，用字符串来表示；得到字符串形式的hash值
+            String md5=new BigInteger(1, md.digest()).toString(16);
+            //BigInteger会把0省略掉，需补全至32位
+            return fillMD5(md5);
+        } catch (Exception e) {
+            throw new RuntimeException("MD5加密错误:"+e.getMessage(),e);
+        }
+    }
+
+    public static String fillMD5(String md5){
+        return md5.length()==32?md5:fillMD5("0"+md5);
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +64,7 @@ public class Signup extends Activity{
         String userName = param[1];
         String userPass = param[2];
         String userPhoneNumber = param[3];
+        userPass = getMD5(userPass);
         //创建异步请求对象
         AsyncHttpClient client = new AsyncHttpClient();
         //输入要请求的url
