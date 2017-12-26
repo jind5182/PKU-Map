ec5c99241e197e4be50994c0d0637e5e43e69807
tonywyb <1500012946@pku.edu.cn>
2017-12-26 18:26:47 +0800

    update

---
+++ app/src/main/java/com/example/macpro/pku_map/Coder.java
@@ -0,0 +1,107 @@
+package com.example.macpro.pku_map;
+import java.security.MessageDigest;
+import javax.crypto.KeyGenerator;
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+public abstract class Coder {
+	public static final String KEY_SHA = "SHA";
+	public static final String KEY_MD5 = "MD5";
+
+	/**
+	 * MAC�㷨��ѡ���¶����㷨
+	 * HmacMD5 
+	 * HmacSHA1 
+	 * HmacSHA256 
+	 * HmacSHA384 
+	 * HmacSHA512
+	 */
+	public static final String KEY_MAC = "HmacMD5";
+
+	/**
+	 * BASE64����
+	 * @param key
+	 * @return
+	 * @throws Exception
+	 */
+	public static byte[] decryptBASE64(String key) throws Exception {
+		return (new BASE64Decoder()).decodeBuffer(key);
+	}
+
+	/**
+	 * BASE64����
+	 * 
+	 * @param key
+	 * @return
+	 * @throws Exception
+	 */
+	public static String encryptBASE64(byte[] key) throws Exception {
+		return (new BASE64Encoder()).encodeBuffer(key);
+	}
+
+	/**
+	 * MD5����
+	 * 
+	 * @param data
+	 * @return
+	 * @throws Exception
+	 */
+	public static byte[] encryptMD5(byte[] data) throws Exception {
+
+		MessageDigest md5 = MessageDigest.getInstance(KEY_MD5);
+		md5.update(data);
+
+		return md5.digest();
+
+	}
+
+	/**
+	 * SHA����
+	 * 
+	 * @param data
+	 * @return
+	 * @throws Exception
+	 */
+	public static byte[] encryptSHA(byte[] data) throws Exception {
+
+		MessageDigest sha = MessageDigest.getInstance(KEY_SHA);
+		sha.update(data);
+
+		return sha.digest();
+
+	}
+
+	/**
+	 * ��ʼ��HMAC��Կ
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
+	public static String initMacKey() throws Exception {
+		KeyGenerator keyGenerator = KeyGenerator.getInstance(KEY_MAC);
+
+		SecretKey secretKey = keyGenerator.generateKey();
+		return encryptBASE64(secretKey.getEncoded());
+	}
+
+	/**
+	 * HMAC����
+	 * 
+	 * @param data
+	 * @param key
+	 * @return
+	 * @throws Exception
+	 */
+	public static byte[] encryptHMAC(byte[] data, String key) throws Exception {
+
+		SecretKey secretKey = new SecretKeySpec(decryptBASE64(key), KEY_MAC);
+		Mac mac = Mac.getInstance(secretKey.getAlgorithm());
+		mac.init(secretKey);
+
+		return mac.doFinal(data);
+
+	}
+}
